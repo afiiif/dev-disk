41bd1ffcd6a733469f1b046d17dff20e544b569f
M. Afifudin <afif.x.udin@gmail.com>
2024-02-04 06:40:42 +0700

    remove tests

--- tests/subscribe.test.tsx
+++
@@ -1,9 +0,0 @@
-import { describe, expect, it } from 'vitest'
-import { create } from 'zustand'
-
-describe('subscribe()', () => {
-  it('should correctly have access to subscribe', () => {
-    const { subscribe } = create(() => ({ value: 1 }))
-    expect(typeof subscribe).toBe('function')
-  })
-})
