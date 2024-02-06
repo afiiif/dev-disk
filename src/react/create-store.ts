658963de0d157a8cb9cb8546bf08dd32266e4071
M. Afifudin <afif.x.udin@gmail.com>
2024-02-06 17:22:06 +0700

    feat: update initStore & createStore

--- src/react/create-store.ts
+++ src/react/create-store.ts
@@ -8,32 +8,33 @@ import {
   initStore,
 } from '../vanilla/store.ts'
 
-type UseStore<T> = {
+export type UseStore<T> = {
   <U = T>(selector: (state: T) => U): U
   use: {
     setInitialValue: (value: SetState<T>) => void
   }
 } & StoreApi<T>
+
 export const createStore = <T extends Record<string, any>>(
   initializer: StoreInitializer<T>,
   options: InitStoreOptions<T> = {},
 ) => {
-  const api = initStore(initializer, options)
+  const store = initStore(initializer, options)
 
   const useStore = <U = T>(selector: (state: T) => U) => {
     const refSelector = useRef(selector)
     const refInitialState = useRef<U>()
     useState(() => {
-      // Prevent re-compute selector(api.getState())
-      refInitialState.current = selector(api.getState())
+      // Prevent re-compute selector(store.get())
+      refInitialState.current = selector(store.get())
     })
 
     const [state, setState] = useState(refInitialState.current)
 
     useEffect(() => {
-      const curr = refSelector.current(api.getState())
+      const curr = refSelector.current(store.get())
       if (!shallow(curr, refInitialState.current)) setState(curr)
-      return api.subscribe((nextState, prevState) => {
+      return store.subscribe((nextState, prevState) => {
         const prev = refSelector.current(prevState)
         const next = refSelector.current(nextState)
         !shallow(prev, next) && setState(next)
@@ -47,17 +48,17 @@ export const createStore = <T extends Record<string, any>>(
     setInitialValue: (value: SetState<T>) => {
       // eslint-disable-next-line react-hooks/rules-of-hooks
       useState(() => {
-        if (api.getSubscribers().size > 0) {
+        if (store.getSubscribers().size > 0) {
           return console.warn(
             'Cannot perform setInitialValues because the store already have subscriber',
           )
         }
-        api.setState(value)
+        store.set(value)
       })
     },
   }
 
-  Object.assign(useStore, api)
+  Object.assign(useStore, store)
   Object.assign(useStore, { use })
 
   return useStore as UseStore<T>
--- src/vanilla/store.ts
+++ src/vanilla/store.ts
@@ -5,19 +5,13 @@ export type SetState<T> = Partial<T> | ((state: T) => Partial<T>)
 export type Subscriber<T> = (state: T, prevState: T) => void
 
 export type StoreApi<T> = {
-  getState: () => T
-  setState: (value: SetState<T>, silent?: boolean) => void
+  get: () => T
+  set: (value: SetState<T>, silent?: boolean) => void
   subscribe: (subscriber: Subscriber<T>) => () => void
   getSubscribers: () => Set<Subscriber<T>>
 }
 
-export type StoreInitializer<T> =
-  | T
-  | ((
-      setState: StoreApi<T>['setState'],
-      getState: StoreApi<T>['getState'],
-      store: StoreApi<T>,
-    ) => T)
+export type StoreInitializer<T> = T | ((store: StoreApi<T>) => T)
 
 export type InitStoreOptions<T> = {
   intercept?: (nextState: T, prevState: T) => void | Maybe<Partial<T>>
@@ -40,12 +34,12 @@ export const initStore = <T extends Record<string, any>>(
   } = options
 
   let state: T
-  const getState = () => state
+  const get = () => state
 
   const subscribers = new Set<Subscriber<T>>()
   const getSubscribers = () => subscribers
 
-  const setState = (value: SetState<T>, silent = false) => {
+  const set = (value: SetState<T>, silent = false) => {
     const prevState = state
     state = { ...state, ...getValue(value, state) }
     if (intercept) state = { ...state, ...intercept(state, prevState) }
@@ -63,8 +57,8 @@ export const initStore = <T extends Record<string, any>>(
     }
   }
 
-  const api = { getState, setState, subscribe, getSubscribers }
-  const initialState = getValue(initializer, setState, getState, api)
+  const store = { get, set, subscribe, getSubscribers }
+  const initialState = getValue(initializer, store)
   state = initialState
-  return api
+  return store
 }
