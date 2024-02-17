53b8301a5f6c608b1bedc1d31f1dd6f866430e95
M. Afifudin <afif.x.udin@gmail.com>
2024-02-18 06:20:29 +0700

    feat: add react createQuery

--- src/react.ts
+++ src/react.ts
@@ -1,4 +1,5 @@
 export * from './react/use-sync-store-slice.ts'
 export * from './react/create-store.ts'
 export * from './react/create-stores.ts'
+export * from './react/create-query.ts'
 export * from './react/create-mutation.ts'
---
+++ src/react/create-query.ts
@@ -0,0 +1,147 @@
+import { InitQueryOptions, Query, QueryState, initQuery } from '../vanilla/query.ts'
+import { getValue, isClient } from '../vanilla/utils.ts'
+import { CreateStoresOptions, createStores } from './create-stores.ts'
+
+// ----------------------------------------
+// Type definitions
+
+export type CreateQueryOptions<T extends Query> = InitQueryOptions<T> &
+  CreateStoresOptions<
+    QueryState<T>,
+    T['key'] extends Record<string, any> ? T['key'] : Record<string, never>
+  > & {
+    fetchOnMount?: boolean | 'always' | ((key: T['key']) => boolean | 'always')
+    fetchOnWindowFocus?: boolean | 'always' | ((key: T['key']) => boolean | 'always')
+    fetchOnReconnect?: boolean | 'always' | ((key: T['key']) => boolean | 'always')
+    keepPreviousData?: boolean
+    gcTime?: number
+  }
+
+// ----------------------------------------
+// Source code
+
+export const createQuery = <T extends Query>(options: CreateQueryOptions<T>) => {
+  const initializer = initQuery(options)
+
+  const defaultFetchOnWindowFocus = options.fetchOnMount ?? true
+  const defaultFetchOnReconnect = options.fetchOnMount ?? true
+  const {
+    fetchOnMount = true,
+    fetchOnWindowFocus = defaultFetchOnWindowFocus,
+    fetchOnReconnect = defaultFetchOnReconnect,
+    refetchInterval = false,
+    keepPreviousData,
+    gcTime = 5 * 60 * 1000, // 5 minutes
+  } = options
+
+  const useQuery = createStores(
+    initializer,
+    (() => {
+      const windowFocusHandler = () => {
+        useQuery.stores.forEach((store) => {
+          if (store.getSubscribers().size === 0) return
+          const result = getValue(fetchOnWindowFocus, store.key)
+          if (result === 'always') store.fetch()
+          else if (result) store.fetch.cacheFirst()
+        })
+      }
+
+      const reconnectHandler = () => {
+        useQuery.stores.forEach((store) => {
+          if (store.getSubscribers().size === 0) return
+          const result = getValue(fetchOnReconnect, store.key)
+          if (result === 'always') store.fetch()
+          else if (result) store.fetch.cacheFirst()
+        })
+      }
+
+      return {
+        ...options,
+
+        // onFirstSubscribe:
+        //  - Run refetch interval
+        //  - Handle fetchOnWindowFocus & fetchOnReconnect
+        //  - Cancel garbage collection timeout
+        onFirstSubscribe: (state) => {
+          const store = useQuery.getStore(state.key)
+          if (state.isSuccess) {
+            const refetchIntervalValue = isClient && getValue(refetchInterval, state)
+            if (refetchIntervalValue) {
+              store.internal.timeout.refetchInterval = window.setTimeout(
+                store.fetch,
+                refetchIntervalValue,
+              )
+            }
+          }
+          if (isClient) {
+            if (fetchOnWindowFocus) window.addEventListener('focus', windowFocusHandler)
+            if (fetchOnReconnect) window.addEventListener('online', reconnectHandler)
+          }
+          clearTimeout(store.internal.timeout.garbageCollection)
+          options.onFirstSubscribe?.(state)
+        },
+
+        // onSubscribe:
+        //  - Handle fetchOnMount
+        onSubscribe: (state) => {
+          const store = useQuery.getStore(state.key)
+          const result = getValue(fetchOnMount, store.key)
+          if (result === 'always') store.fetch()
+          else if (result) store.fetch.cacheFirst()
+          options.onSubscribe?.(state)
+        },
+
+        // onLastUnsubscribe:
+        //  - Start garbage collection timeout
+        //  - Cancel retry, retryNextPage, refetchInterval
+        //  - Reset retry counter
+        //  - Detach window event handler
+        //    (disable fetchOnWindowFocus & fetchOnReconnect since it has no subscribers anymore)
+        onLastUnsubscribe: (state) => {
+          let totalSubs = 0
+          useQuery.stores.forEach((store) => {
+            if (store.getSubscribers()) totalSubs++
+          })
+          if (isClient && totalSubs === 0) {
+            if (fetchOnWindowFocus) window.removeEventListener('focus', windowFocusHandler)
+            if (fetchOnReconnect) window.removeEventListener('online', reconnectHandler)
+          }
+          const store = useQuery.getStore(state.key)
+          store.set({ retryCount: 0, retryNextPageCount: 0 })
+          clearTimeout(store.internal.timeout.retry)
+          clearTimeout(store.internal.timeout.retryNextPage)
+          clearTimeout(store.internal.timeout.refetchInterval)
+          if (isClient && gcTime !== Infinity) {
+            store.internal.timeout.garbageCollection = window.setTimeout(store.reset, gcTime)
+          }
+          options.onLastUnsubscribe?.(state)
+        },
+
+        // onBeforeChangeKey:
+        //  - Handle keepPreviousData
+        onBeforeChangeKey: (nextKey, prevKey) => {
+          const pStore = useQuery.getStore(prevKey)
+          const nStore = useQuery.getStore(nextKey)
+          if (keepPreviousData) {
+            const nextData = nStore.get()
+            const prevData = pStore.get()
+            if (prevData.data && !nextData.data) {
+              nStore.set({
+                status: 'success',
+                isLoading: false,
+                isSuccess: true,
+                isError: false,
+                data: prevData.data,
+                response: prevData.response,
+                isPreviousData: true,
+              })
+            }
+          }
+          options.onBeforeChangeKey?.(nextKey, prevKey)
+        },
+      }
+    })(),
+  )
+
+  return useQuery
+}
