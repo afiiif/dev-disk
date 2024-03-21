a9f1ea8ef8d40e71114e1804ea10daa35c8a117b
M. Afifudin <afif.x.udin@gmail.com>
2024-03-21 08:15:08 +0700

    feat: use `useLayoutEffect` for subscription

--- src/react/use-sync-store-slice.ts
+++ src/react/use-sync-store-slice.ts
@@ -1,78 +1,48 @@
-import { useCallback, useDebugValue, useMemo, useRef, useSyncExternalStore } from 'react'
+import { useRef, useState } from 'react'
 import { StoreApi, identity, shallow } from 'dev-disk'
-
-const useMemoShallowSelector = <TState, TStateSlice>(
-  getState: () => TState,
-  selector: (state: TState) => TStateSlice,
-) => {
-  const selectorRef = useRef(selector)
-  selectorRef.current = selector
-  return useMemo(() => {
-    let slice: TStateSlice
-    return () => {
-      const state = getState()
-      const nextSlice = selectorRef.current(state)
-      if (!shallow(slice, nextSlice)) slice = nextSlice
-      return slice
-    }
-  }, [getState])
-}
+import { useIsomorphicLayoutEffect } from './use-isomorphic-layout-effect.ts'
 
 export const useSyncStoreSlice = <TState extends Record<string, any>, TStateSlice>(
   store: StoreApi<TState>,
   selector: (state: TState) => TStateSlice = identity as (state: TState) => TStateSlice,
 ) => {
-  const slice = useSyncExternalStore<TStateSlice>(
-    store.subscribe,
-    useMemoShallowSelector(store.get, selector),
-    useMemoShallowSelector(store.getInitial, selector),
+  const [, reRender] = useState({})
+
+  const selectorRef = useRef(selector)
+  selectorRef.current = selector
+
+  useIsomorphicLayoutEffect(
+    () =>
+      store.subscribe((state, prevState) => {
+        const prevSlice = selectorRef.current(prevState)
+        const nextSlice = selectorRef.current(state)
+        if (!shallow(prevSlice, nextSlice)) reRender({})
+      }),
+    [store],
   )
-  useDebugValue(slice)
-  return slice
+
+  return selector(store.get())
 }
 
 export const useSyncStoresSlice = <TState extends Record<string, any>, TStateSlice>(
   stores: StoreApi<TState>[],
   selector: (state: TState) => TStateSlice = identity as (state: TState) => TStateSlice,
 ) => {
+  const [, reRender] = useState({})
+
   const selectorRef = useRef(selector)
   selectorRef.current = selector
 
-  const slices = useSyncExternalStore<TStateSlice[]>(
-    useCallback(
-      (onStoreChange) => {
-        const unsubscribeFns = stores.map((store) => store.subscribe(onStoreChange))
-        return () => {
-          unsubscribeFns.forEach((unsubscribe) => unsubscribe())
-        }
-      },
-      [stores],
-    ),
-    useMemo(() => {
-      let slices: TStateSlice[]
-      return () => {
-        const nextSlices = []
-        let isEqual = true
-        for (let i = 0; i < stores.length; i++) {
-          nextSlices[i] = selectorRef.current(stores[i].get())
-          if (!shallow(slices[i], nextSlices[i])) isEqual = false
-        }
-        return isEqual ? slices : nextSlices
-      }
-    }, [stores]),
-    useMemo(() => {
-      let slices: TStateSlice[]
-      return () => {
-        const nextSlices = []
-        let isEqual = true
-        for (let i = 0; i < stores.length; i++) {
-          nextSlices[i] = selectorRef.current(stores[i].getInitial())
-          if (!shallow(slices[i], nextSlices[i])) isEqual = false
-        }
-        return isEqual ? slices : nextSlices
-      }
-    }, [stores]),
-  )
-  useDebugValue(slices)
-  return slices
+  useIsomorphicLayoutEffect(() => {
+    const unsubscribeFns = stores.map((store) =>
+      store.subscribe((state, prevState) => {
+        const prevSlice = selectorRef.current(prevState)
+        const nextSlice = selectorRef.current(state)
+        if (!shallow(prevSlice, nextSlice)) reRender({})
+      }),
+    )
+    return () => unsubscribeFns.forEach((unsubscribe) => unsubscribe())
+  }, [stores])
+
+  return stores.map(({ get }) => selector(get()))
 }
